cf50ad0383fc76ea3bc21e8924b0eef0c3869d31
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-11-18 08:35:04 +0000

    Deploying to gh-pages from @ DawidKrupski/goit-js-hw-09@8a8ccb153932ab0b891e55b31d06a557ed8723ed 🚀

---
+++ 01-color-switcher.ef50ee38.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),[l,t]=document.querySelectorAll("button");let d=null;t.disabled=!0;l.addEventListener("click",(()=>{l.disabled=!0,t.disabled=!1,d=setInterval((()=>{const l=()=>`#${Math.floor(16777215*Math.random()).toString(16)}`;console.log(l()),e.style.backgroundColor=l()}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),l.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.ef50ee38.js.map
