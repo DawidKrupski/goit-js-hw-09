8a8ccb153932ab0b891e55b31d06a557ed8723ed
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-18 09:34:30 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const [startBtn, stopBtn] = document.querySelectorAll('button');
 
+let colorInterval = null;
+stopBtn.disabled = true;
+
+const startColorChange = () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  colorInterval = setInterval(() => {
+    const getRandomHexColor = () => {
+      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    };
+    console.log(getRandomHexColor());
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const stopColorChange = () => {
+  clearInterval(colorInterval);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
