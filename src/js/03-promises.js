7a2e83a79b75a5006ad64e40c0b25d0aba311f8c
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-23 13:36:12 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const submit = document.querySelector(`[type="submit"]`);
+const firstDelay = document.querySelector(`[name="delay"]`);
+const delayStep = document.querySelector(`[name="step"]`);
+const amount = document.querySelector(`[name="amount"]`);
+
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve('success');
+    } else {
+      reject('failure');
+    }
+  });
 }
+
+const send = event => {
+  event.preventDefault();
+  setTimeout(() => {
+    for (i = 0; i < amount.value; i++) {
+      let position = i + 1;
+      let delay = delayStep.value * i + Number(firstDelay.value);
+      setTimeout(() => {
+        createPromise()
+          .then(() => {
+            Notiflix.Notify.success(
+              `✅ Fulfilled promise ${position} in ${delay}ms`
+            );
+          })
+          .catch(() => {
+            Notiflix.Notify.failure(
+              `❌ Rejected promise ${position} in ${delay}ms`
+            );
+          });
+      }, delay);
+    }
+  }, firstDelay.value);
+};
+
+submit.addEventListener('click', send);
